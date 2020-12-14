983510d414071c63b7d5850cb8aa2959a9271258
Nico De Witte <nico.dewitte@outlook.com>
2020-12-14 09:35:39 +0100

    Make methods of decoder static

--- Fluvius_P1_FeatherFirmware_v3/decoder.h
+++ Fluvius_P1_FeatherFirmware_v3/decoder.h
@@ -1,14 +1,15 @@
 #pragma once
-#include <Arduino.h>
+
+// #include <Arduino.h>
 #include "datagram.h"
 
 namespace SmartMeter {
   class Decoder {
       public:
-        Datagram decode(char* buffer, size_t bufferlength);
+        static Datagram decode(char* buffer, size_t bufferlength);
 
       private:
-        double ParseDataValue(char* _buffer, char* key, int datablock);
+        static double ParseDataValue(char* _buffer, char* key, int datablock);
 
 
   };

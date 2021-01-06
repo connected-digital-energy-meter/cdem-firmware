2489c5883368d2b0e162f7d6d137037e0be9e739
Nico De Witte <nico.dewitte@outlook.com>
2021-01-06 07:37:31 +0100

    Make sure device config is saved after boot wizard is stepped through. This closes #12

--- Fluvius_P1_FeatherFirmware_v3/src/boot/boot_manager.cpp
+++ Fluvius_P1_FeatherFirmware_v3/src/boot/boot_manager.cpp
@@ -16,20 +16,18 @@ namespace SmartMeter {
       SerialDebug.println("Loaded existing configuration");
     } else {
       SerialDebug.println("No existing configuration could be loaded");
-      SerialDebug.println("Saving factory default configuration");
+      SerialDebug.println("Booting configuration wizard ...");
       configManager.factory_default();
       
       BootWizard bootWizard(configManager.current_config(), &SerialDebug);
       Configuration config = bootWizard.RunWizard();
-      
-      if (config != this->configManager.current_config()) {
-        SerialDebug.println("Device configuration has changed. Saving it ...");
-        configManager.current_config(config);
-        save_config();
-      } else {
-        SerialDebug.println("Device configuration is not altered. No need for saving it.");
-      }
 
+      // This point will only be reached once the user has stepped through
+      // the configuration wizard. So here we have to save.
+      SerialDebug.println("Saving device configuration ...");
+      configManager.current_config(config);
+      save_config();
+      delay(1000);
     }
 
     SerialDebug.println("Hold the touch if you wish to boot into boot menu");
@@ -51,7 +49,7 @@ namespace SmartMeter {
 
   void BootManager::show_boot_menu(void) {
     BootConfig bootConfig(configManager.current_config(), &SerialDebug);
-    Configuration config = bootConfig.enable_boot_menu();    // TODO - Save config if newer
+    Configuration config = bootConfig.enable_boot_menu();
 
     if (config != this->configManager.current_config()) {
       SerialDebug.println("Device configuration has changed. Saving it ...");
@@ -66,7 +64,7 @@ namespace SmartMeter {
     if (configManager.save_configuration()) {
       SerialDebug.println("Successfully saved configuration");
     } else {
-      SerialDebug.println("Something went wrong. Could not save configuration");
+      SerialDebug.println("Something went wrong. Could not save configuration.");
     }
   }
 

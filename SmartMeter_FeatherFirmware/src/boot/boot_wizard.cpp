989875f3c1a1fb176bc1ccbb52269f9f73464f4f
Nico De Witte <nico.dewitte@outlook.com>
2021-02-26 14:49:12 +0100

    Fix some typos in the configuration wizard

--- SmartMeter_FeatherFirmware/src/boot/boot_wizard.cpp
+++ SmartMeter_FeatherFirmware/src/boot/boot_wizard.cpp
@@ -15,19 +15,20 @@ namespace SmartMeter {
 
     Configuration BootWizard::RunWizard(void) {
         userSerial->println("");
-        userSerial->println("#############################");
-        userSerial->println("# Smart Meter Config Wizard #");
-        userSerial->println("#############################");
+        userSerial->println("#########################################################");
+        userSerial->println("# Connected Digital Energy Meter - Configuration Wizard #");
+        userSerial->println("#########################################################");
         userSerial->println("");
-        userSerial->println("First let us configure your Wifi connection.");
-        userSerial->println("--------------------------------------------");
+        userSerial->println("------------------------------------------");
+        userSerial->println("Let us first configure your WiFi settings:");
+        userSerial->println("------------------------------------------");
         userConfigInputHelper.configure_wifi_ssid(&newConfig);
         userSerial->println("");
         userConfigInputHelper.configure_wifi_password(&newConfig);
         userSerial->println("");
-        userSerial->println("------------------------------------------");
-        userSerial->println("Next we configure your Network connection.");
-        userSerial->println("------------------------------------------");
+        userSerial->println("-------------------------------------------");
+        userSerial->println("Next configure your local network settings:");
+        userSerial->println("-------------------------------------------");
         userConfigInputHelper.configure_use_dhcp(&newConfig);
         if (!newConfig.use_dhcp()) {
           userSerial->println("");
@@ -38,21 +39,26 @@ namespace SmartMeter {
           userConfigInputHelper.configure_default_gateway(&newConfig);
         }
         userSerial->println("");
-        userSerial->println("------------------------------------------------------------------------------");
-        userSerial->println("This device will post it's data to a MQTT broker.");
-        userSerial->println("In order to do this we will need the IP-address and port of your MQTT broker.");
-        userSerial->println("We also need the base topic in your MQTT broker where we should post the data.");
-        userSerial->println("------------------------------------------------------------------------------");
+        userSerial->println("----------------------------------------------------------------------------");
+        userSerial->println("This device will publish it's data to an MQTT broker.");
+        userSerial->println("In order to do so, it will need the IP address and port of your MQTT broker.");
+        userSerial->println("It also needs a base topic where the data can be published on.");
+        userSerial->println("----------------------------------------------------------------------------");
         userConfigInputHelper.configure_mqtt_broker(&newConfig);
         userSerial->println("");
         userConfigInputHelper.configure_mqtt_broker_port(&newConfig);
         userSerial->println("");
         userConfigInputHelper.configure_mqtt_topic(&newConfig);
         userSerial->println("");
-        userSerial->println("---------------------------------------------------------------------------------");
-        userSerial->println("Finally we need the read period you want to publish your data to the MQTT broker.");
-        userSerial->println("---------------------------------------------------------------------------------");
+        userSerial->println("----------------------------------------------------------");
+        userSerial->println("Finally, we need to configure the time between data reads.");
+        userSerial->println("----------------------------------------------------------");
         userConfigInputHelper.configure_device_read_period(&newConfig);
+        userSerial->println("");
+        userSerial->println("Thank you for using the configuration wizard ...");
+        userSerial->println("");
+        userSerial->println("");
+
         return newConfig;
     }
 
